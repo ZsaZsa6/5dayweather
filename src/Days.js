474de2084128821954680c79645b5006bcadd99a
Ronda Cowen <cowenhome5@gmail.com>
2021-11-03 21:17:26 -0500

    work on API call

--- src/Days.js
+++ src/Days.js
@@ -1,4 +1,13 @@
 import React from 'react';
+
+const baseUrl = 'http://openlibrary.org';
+
+export function highsLows(query) {
+    const url = (https://api.openweathermap.org/data/2.5/onecall?lat={34.720902}&lon={-96.919178}&exclude={current,minutely,hourly,alerts}&appid={ba1e83d61cbe3a4871a33ffc3893e95a});
+   
+
+    return fetch(url).then(response => response.json());
+}
 export function Days() {
     return(
         <div>
