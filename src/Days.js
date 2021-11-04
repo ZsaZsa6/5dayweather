ceb2700e493078337967cb4be3ac2339e5a497aa
Ronda Cowen <cowenhome5@gmail.com>
2021-11-03 22:37:49 -0500

    fix API query

--- src/Days.js
+++ src/Days.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-const baseUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={34.720902}&lon={-96.919178}&units={imperial}&exclude={current,minutely,hourly,alerts}&appid={ba1e83d61cbe3a4871a33ffc3893e95a}';
+const baseUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=34.720902&lon=-96.919178&units=imperial&exclude=current,minutely,hourly,alerts&appid=ba1e83d61cbe3a4871a33ffc3893e95a';
 
 export function highsLows() {   
 
