bd361d361c0799239be4ffd182f98e5c8c6150d9
Ronda Cowen <cowenhome5@gmail.com>
2021-11-13 11:54:46 -0600

    remove app and add content to Days

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
+import Days from './Days';
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Days />, document.getElementById('root'));
 
