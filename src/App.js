995e9192d9e593dc94f3d03e337a27365cbbc9f6
Ronda Cowen <cowenhome5@gmail.com>
2021-11-13 13:00:41 -0600

    updated dependencies, install axios

--- src/App.js
+++ src/App.js
@@ -1,19 +1,1 @@
 
-import './App.css';
-import { Days } from './Days';
-
-
-function App() {
-  return (
-    <div className="App">
-      <header>
-       
-        <h1>5 Day Weather Report for my House</h1>
-        
-      </header>
-      <Days/>
-    </div>
-  );
-}
-
-export default App;
