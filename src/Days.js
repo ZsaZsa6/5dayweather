02958b1219ae57cc2b88c452e5b791355606c889
Ronda Cowen <cowenhome5@gmail.com>
2021-11-27 19:15:26 -0600

    work on URL and params

--- src/Days.js
+++ src/Days.js
@@ -21,17 +21,13 @@ const Days = () => {
             <input type="text"className="search"placeholder="Search..."value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
             {weather.main && (
                 <div className="city">
-                    <h2 className="city-name">
-                        <span>{weather.name}</span>
-                        <sup>{weather.sys.country}</sup>
-                    </h2>
-                    <div className="city-temp">
+                   
+                    <div className="avg-temp">
                         {Math.round(weather.main.temp)}
-                        <sup>&deg;C</sup>
+                        <sup>&deg;F</sup>
                     </div>
                     <div className="info">
-                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
-                        <p>{weather.weather[0].description}</p>
+                        
                     </div>
                 </div>
             )}
--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const URL = 'https://api.openweathermap.org/data/2.5/onecall';
+const URL = http://api.openweathermap.org/data/2.5/timemachine?lat=60.99&lon=30.9&dt=1586468027&appid='ba1e83d61cbe3a4871a33ffc3893e95a' 
+
 const API_KEY = 'ba1e83d61cbe3a4871a33ffc3893e95a';
 
-export const fetchWeather = async (query) => {
+export const fetchWeather = {
     const { data } = await axios.get(URL, {
         params: {
             q: query,
@@ -16,5 +17,6 @@ export const fetchWeather = async (query) => {
     });
 
     return data;
-}
 
+
+https://api.openweathermap.org/data/2.5/onecall?lat=34.720902&lon=-96.919178,&exclude=currently,minutely,hourly,alerts&appid='ba1e83d61cbe3a4871a33ffc3893e95a'
