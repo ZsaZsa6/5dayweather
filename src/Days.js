07ee063e92aed8b093badcef88334e481a5956fa
Ronda Cowen <cowenhome5@gmail.com>
2021-12-03 15:47:16 -0600

    working for console.log of response

--- src/Days.js
+++ src/Days.js
@@ -5,9 +5,9 @@ import './App.css';
 const URL = 'https://api.openweathermap.org/data/2.5/onecall?lat=34.720902&lon=-96.919178&exclude=current,hourly,minutely,alerts';
 
 const Days = () => {
-    axios.get(URL, { 
+     axios.get(URL, { 
         params: {
-        APP_ID: 'ba1e83d61cbe3a4871a33ffc3893e95a'
+        appid: 'ba1e83d61cbe3a4871a33ffc3893e95a'
     }
     })
     .then(function (response) {
