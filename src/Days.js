e9765575c0681fd26f94bc9966d5fa91e8abf5a1
Ronda Cowen <cowenhome5@gmail.com>
2021-12-03 15:36:36 -0600

    work on URL and params for call

--- src/Days.js
+++ src/Days.js
@@ -1,37 +1,27 @@
-import React, { useState } from 'react';
 import axios from 'axios';
 
 
 import './App.css';
+const URL = 'https://api.openweathermap.org/data/2.5/onecall?lat=34.720902&lon=-96.919178&exclude=current,hourly,minutely,alerts';
 
 const Days = () => {
-    axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=34.720902&lon=-96.919178&exclude=current,hourly,minutely,alerts&appid=APP_ID', 
-    { params: {
+    axios.get(URL, { 
+        params: {
         APP_ID: 'ba1e83d61cbe3a4871a33ffc3893e95a'
     }
     })
-    
+    .then(function (response) {
+        console.log(response);
+      })
+
     
    
-    }
+    
 
     return (
-        <div className="main-container">
-            <input type="text"className="search"placeholder="Search..."value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
-            {weather.main && (
-                <div className="city">
-                   
-                    <div className="avg-temp">
-                        {Math.round(weather.main.temp)}
-                        <sup>&deg;F</sup>
-                    </div>
-                    <div className="info">
-                        
-                    </div>
-                </div>
-            )}
+        <div >
         </div>
     );
-}
 
-export default Days;
+    }
+export default Days
