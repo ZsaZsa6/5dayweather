c999766e67936fab4468f0a6fdaf75e596ae40ad
Ronda Cowen <cowenhome5@gmail.com>
2021-12-03 20:42:56 -0600

    syntax error

--- src/Days.js
+++ src/Days.js
@@ -1,29 +1,23 @@
 import axios from 'axios';
-
+import React, { useState } from 'react';
 
 import './App.css';
-const URL = 'https://api.openweathermap.org/data/2.5/onecall?lat=34.720902&lon=-96.919178&exclude=current,hourly,minutely,alerts&units=imperial';
+const URL = 'https://api.openweathermap.org/data/2.5/onecall?lat=34.720902&lon=-96.919178&exclude=current,hourly,minutely,alerts&units=imperial'
 
 const Days = () => {
+    let [responseObj, setResponseObj] = useState({});
      axios.get(URL, { 
         params: {
         appid: 'ba1e83d61cbe3a4871a33ffc3893e95a'
     }
-    })
-    .then(JSON.stringify(response))
-}
-    
-      
-
-   
-    
+        .then(response => {
+           setResponseObj(response)})}     
+     
 
     return (
         <div >
-         {JSON.stringify(response)}
+         {JSON.stringify(responseObj)}
            
         </div>
     );
-
     }
-export default Days
