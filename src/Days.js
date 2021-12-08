8929ae7f4431371ad299b08feb9ec79ca54a0daa
Ronda Cowen <cowenhome5@gmail.com>
2021-12-08 14:47:28 -0600

    adjusting response

--- src/Days.js
+++ src/Days.js
@@ -2,17 +2,22 @@ import axios from 'axios';
 import React, { useState } from 'react';
 
 import './App.css';
-const URL = 'https://api.openweathermap.org/data/2.5/onecall?lat=34.720902&lon=-96.919178&exclude=current,hourly,minutely,alerts&units=imperial'
+const URL = 'https://api.openweathermap.org/data/2.5/onecall?'
 
 const Days = () => {
     let [responseObj, setResponseObj] = useState({});
      axios.get(URL, { 
         params: {
-        appid: 'ba1e83d61cbe3a4871a33ffc3893e95a'
+        appid: 'ba1e83d61cbe3a4871a33ffc3893e95a',
+        lat: '34.720902',
+        lon: '-96.919178',
+        exclude: 'current,hourly,minutely,alerts',
+        units: 'imperial'
     }
         .then(response => {
-           setResponseObj(response)})}     
-     
+           setResponseObj(response)})    
+        },
+    
 
     return (
         <div >
@@ -21,3 +26,4 @@ const Days = () => {
         </div>
     );
     }
+
